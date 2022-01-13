1acea0bafdbaf4d31e400bbab8b2831e87a4e73f
WINTHANG <haxuanthang29495@gmail.com>
2022-01-13 23:03:23 +0900

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import { RiMenu3Fill, RiCloseLine } from "react-icons/ri";
 import logo from '../../assets/logo.svg';
 
+
+const Menu = () => (
+    <>
+        <p><a href="#home"></a>Home</p>
+        <p><a href="#wgpt3"></a>What is GPT?</p>
+        <p><a href="#possibility"></a>Open AI</p>
+        <p><a href="#features"></a>Case Studies</p>
+        <p><a href="#blog"></a>Library</p>
+    </>
+)
+
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
     return (
-        <div className='gpt__navbar'>
-            <div className='gpt__navbar-links'>
-                <div className='gpt__navbar-links_logo'>
+        <div className="gpt3__navbar">
+            <div className="gpt3__navbar-links">
+                <div className="gpt3__navbar-links_logo">
                     <img src={logo} alt="gpt-logo" />
                 </div>
+                <div className='gpt3__navbar-links_container'>
+                    <Menu />
+                </div>
+            </div>
+            <div className='gpt3__navbar-sign'>
+                <p>Sign in</p>
+                <button type='button'>Sign up</button>
+            </div>
+            <div className='gpt3__navbar-menu'>
+                {toggleMenu
+                    ? <RiCloseLine color='#fff' size={27} onClick={() => (false)} />
+                    : <RiMenu3Fill color='#fff' size={27} onClick={() => setToggleMenu(true)} />
+                }
+                {toggleMenu && (
+                    <div className='gpt3__navbar-menu_container scale-up-center'>
+                        <div className='gpt3__navbar-menu_container-links'>
+                            <Menu />
+                            <div className='gpt3__navbar-container-links-sign'>
+                                <p>Sign in</p>
+                                <button type='button'>Sign up</button>
+                            </div>
+                        </div>
+                    </div>
+                )
+
+                }
             </div>
         </div>
     )
