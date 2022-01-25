e60f4b9a9f765ebb98f9d650912742eece089912
WINTHANG <haxuanthang29495@gmail.com>
2022-01-25 16:47:43 +0900

    possibility

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -5,9 +5,10 @@ const Features = ({ title, text }) => {
     return (
         <div className='gpt3__features-container__feature'>
             <div className='gpt3__features-container__feature-title'>
+                <div />
                 <h1>{title}</h1>
             </div>
-            <div className='gpt3__features-container-feature-text'>
+            <div className='gpt3__features-container__feature-text'>
                 <p>{text}</p>
             </div>
         </div>
